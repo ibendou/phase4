287b2161cb0e262deedadab45fe7fe8ce556c757
Imane Bendou <ibendou@andrew.cmu.edu>
2019-04-02 18:55:37 +0000

    Fixed the label overlap while editing

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   resources :stores
   resources :employees
   root 'welcome#index'
-  get "active" , to: "employees#active" , as: :employee_active
-  get "inactive" , to: "employees#inactive" , as: :employee_inactive
+  get "active", to: "employees#active", as: :employee_active
+  get "inactive", to: "employees#inactive", as: :employee_inactive
   
-  get "active_" , to: "stores#active" , as: :store_active
-  get "inactive_" , to: "stores#inactive" , as: :store_inactive
+  get "active_", to: "stores#active", as: :store_active
+  get "inactive_", to: "stores#inactive", as: :store_inactive
   
-  get "current" , to: "assignments#current" , as: :assignment_current
-  get "past" , to: "assignments#past" , as: :assignment_past
+  get "current", to: "assignments#current", as: :assignment_current
+  get "past", to: "assignments#past", as: :assignment_past
 end
